6a7fc3be7c250808f0745c4ffe715687f2724dbe
glitch9 <josh_presley@hotmail.com>
2024-09-18 21:15:14 +0000

    C

--- azure-out-tf/main.tf
+++ azure-out-tf/main.tf
@@ -21,13 +21,13 @@ resource "azurerm_resource_group" "rg" {
 
 # this is now an "input" value 
 variable "loc" {
-  type = string
+  type    = string
   default = "eastus2"
 }
 
 # return a value returned by our module
 output "rg_id" {
-  value = azurerm_resource_group.rg.id
+  value       = azurerm_resource_group.rg.id
   description = "ID of the resource group"
 }
 
